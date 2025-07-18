aebc3695e8213ee88359f96130d01591525d27e3
Lior Kozai <149812806+LKozai@users.noreply.github.com>
2025-07-18 13:31:27 -0400

    Update experience.js

--- server/routes/experience.js
+++ server/routes/experience.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const experience = [
   { company: 'theScore', role: 'Associate News Editor', duration: 'Jun. 2021 - Aug. 2023' },
-  { company: 'On The Record', role: 'Copy Editor', duration: 'Jan. 2020 - April 2020' },
   { company: 'TSN.ca', role: 'Web Producer (Intern)', duration: 'Feb. - Mar. 2020' },
   { company: 'Starbucks', role: 'Barista', duration: 'Jun. 2018 - Mar. 2020' }
 ];
