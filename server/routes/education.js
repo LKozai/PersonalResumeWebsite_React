ff4482055cda187b6904a209a3ece92715a17b45
Lior Kozai <149812806+LKozai@users.noreply.github.com>
2025-07-18 13:29:18 -0400

    Update education.js

--- server/routes/education.js
+++ server/routes/education.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const education = [
-  { school: 'Humber Polytechnic', degree: 'Computer Programming, Advanced Diploma Co-Op Program', dates: 'Sept. 2023 - Aug. 2025' },
+  { school: 'Humber Polytechnic', degree: 'Computer Programming, Advanced Diploma Program', dates: 'Sept. 2023 - Apr. 2025' },
   { school: 'Toronto Metropolitan University', degree: 'Bachelor of Journalism', dates: 'Sept. 2016 - Apr. 2020' }
 ];
 
