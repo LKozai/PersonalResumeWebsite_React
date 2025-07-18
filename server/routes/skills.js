f1b0bbccbe5a3fccdf5ecc94670c8a41a0c884e0
Lior Kozai <149812806+LKozai@users.noreply.github.com>
2025-07-18 13:34:07 -0400

    Update skills.js

--- server/routes/skills.js
+++ server/routes/skills.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const skills = [
-  { skill: 'Strong programming skills in languages including Java, JavaScript, React JS (with use of APIs), HTML, CSS, and SQL (with an understanding of relational databases)' },
+  { skill: 'Proficient in programming languages including Java, Python, JavaScript, ReactJS and React Native (with API integration), HTML and CSS, and SQL Developer (relational databases)' },
   { skill: 'Ability to connect, navigate and use Linux/Unix operating systems' },
-  { skill: 'Excellent oral and written communication skills, enhanced by my journalism background' },
+  { skill: 'Strong communicator with a background in journalism' },
   { skill: 'Good problem solver and fast learner, able to adapt to adverse situations' },
-  { skill: 'Organizational skills developed through years of experience completing a degree while working part-time' },
-  { skill: 'Superb attention to detail, with focus on accuracy in coding and writing' }
+  { skill: 'Excellent time management, adaptability, and problem-solving skills' },
+  { skill: 'Superb attention to detail, with focus on accuracy in coding' }
 ];
 
 router.get('/', (req, res) => {
